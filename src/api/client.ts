387a5babb8cfac3c88f9dea8de7513f030a28a9f
Charlottttttttttttte <742850623@qq.com>
2026-05-23 17:00:14 +0800

    Refactor cloudbase initialization and exports

--- src/api/client.ts
+++ src/api/client.ts
@@ -2,26 +2,11 @@ import cloudbase from '@cloudbase/js-sdk';
 
 const ENV_ID = 'english-study-d5guzlwvs975d1521';
 
-let _app: any = null;
+cloudbase.init({
+  env: ENV_ID,
+});
 
-function getApp() {
-  if (!_app) {
-    _app = cloudbase.init({
-      env: ENV_ID,
-    });
-  }
-  return _app;
-}
-
-export const db = {
-  collection: (name: string) => getApp().database().collection(name),
-};
-
-export const tcb = {
-  database: () => getApp().database(),
-  uploadFile: (options: any) => getApp().uploadFile(options),
-  getTempFileURL: (options: any) => getApp().getTempFileURL(options),
-};
-
-export const app = { get: getApp };
+export const db = cloudbase.database();
+export const tcb = cloudbase;
+export { cloudbase as app };
 export const ENV = ENV_ID;
